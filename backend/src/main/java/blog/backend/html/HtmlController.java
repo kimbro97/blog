c2a03e8905e1118ad40059c36d6d03f54fee796f
kimbro97 <rlagudwog@gmail.com>
2023-06-07 20:49:47 +0900

    Configuration 추가ㅣ

--- backend/src/main/java/blog/backend/html/HtmlController.java
+++ backend/src/main/java/blog/backend/html/HtmlController.java
@@ -5,14 +5,22 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
-@Controller
 public class HtmlController implements ErrorController {
 
     @GetMapping("/error")
-    public String redirectRoot() {
+    public String redirectRoot(HttpServletRequest request) {
         log.info("index");
+        System.out.println(request.getRequestURI());
         return "index.html";
+    }
+
+    @GetMapping("/console/**")
+    public String redirectRConsole() {
+        log.info("console");
+        return "console.html";
     }
 
     public String getErrorPath() {
