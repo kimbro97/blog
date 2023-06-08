8ff6e57d7be8bb8658b65f33b5af70d2202a8b2e
kimbro97 <rlagudwog@gmail.com>
2023-06-08 22:11:31 +0900

    HtmlController 추가

--- backend/src/main/java/blog/backend/html/HtmlController.java
+++ backend/src/main/java/blog/backend/html/HtmlController.java
@@ -1,29 +1,24 @@
 package blog.backend.html;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Slf4j
+@Controller
 public class HtmlController implements ErrorController {
-
     @GetMapping("/error")
-    public String redirectRoot(HttpServletRequest request) {
-        log.info("index");
-        System.out.println(request.getRequestURI());
+    public String getIndexHtml(HttpServletRequest request) {
+        String contextPath = request.getServletPath();
+        System.out.println(contextPath);
         return "index.html";
     }
 
     @GetMapping("/console/**")
-    public String redirectRConsole() {
-        log.info("console");
+    public String getConsoleHtml(HttpServletRequest request) {
+        String contextPath = request.getServletPath();
+        System.out.println(contextPath);
         return "console.html";
-    }
-
-    public String getErrorPath() {
-        return null;
     }
 }
