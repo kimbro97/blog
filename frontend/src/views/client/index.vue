841fb988d3e649baebc50596c334b102c5fdee17
kimbro97 <rlagudwog@gmail.com>
2023-06-12 23:21:05 +0900

    작업

--- frontend/src/views/client/index.vue
+++ frontend/src/views/client/index.vue
@@ -4,11 +4,9 @@
 
 <script >
 import mainSection from "@/components/client/main/MainSection.vue";
-import { RouterView } from "vue-router"
 export default {
   components: {
     mainSection,
-    RouterView
   },
   computed: {
 
