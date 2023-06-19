2349993c71821739b07bcf19ea0c1fcbba990a6b
kimbro97 <rlagudwog@gmail.com>
2023-06-19 22:51:13 +0900

    파일 업로드 작업

--- backend/src/main/java/blog/backend/common/upload/UploadController.java
+++ backend/src/main/java/blog/backend/common/upload/UploadController.java
@@ -1,18 +1,34 @@
 package blog.backend.common.upload;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class UploadController {
 
     @PostMapping("/upload")
-    public String fileUpload() {
-        String path = "";
-        return path;
+    public String fileUpload(@RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
+        log.info("request={}", request);
+        log.info("file={}", file);
+        String fullPath = "";
+        if (!file.isEmpty()) {
+            fullPath = fullPath + "/main/resources/static/post/" + file.getOriginalFilename();
+            log.info("파일 저장 fullPath={}", fullPath);
+            file.transferTo(new File(fullPath));
+        }
+
+        return fullPath;
     }
 
 }
